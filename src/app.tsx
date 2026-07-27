cdb1cdc160a7a5726cfedb4f473b4dd8212af947
Gabriela Sinastre <55140599+GabrielaSinastre@users.noreply.github.com>
2026-07-26 23:03:09 -0300

    feat: add the components with tailwindcss styles

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,9 @@
-export function App() {
+import { UploadWidget } from "./components/upload-widget";
 
+export function App() {
   return (
-    <h1>helo </h1>
-  )
+    <main className="h-dvh flex flex-col items-center justify-center p-10">
+      <UploadWidget />
+    </main>
+  );
 }
